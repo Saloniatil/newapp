7033613bde019a875da46625a8ce2a6e1f3efa89
saloniTech <salonipatil10122001@gmail.com>
2024-09-09 16:37:39 +0530

    event management implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,23 @@
+import Search from '@/components/shared/Search';
 import { getAllEvents } from '../../lib/actions/event.action';
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
- 
-export default async function Home() {
+import { SearchParamProps } from '@/types';
+import CategoryFilter from '@/components/shared/CategoryFilter ';
+
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
   const events = await getAllEvents({
-    query:'',
-    category: '',
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6
   })
-
-  console.log(events)
   
   return (
     <>
@@ -42,18 +47,18 @@ export default async function Home() {
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search 
-          CategoryFilter 
+          <Search  />
+          <CategoryFilter />
         </div>
-
+        
         <Collection  
           data={events?.data}
           emptyTitle="No events found"
           emptyStateSubtext="Come back later"
           collectionType= "All_Events"
           limit={6}
-          page={1}
-          totalPages={2}
+          page={page}
+          totalPages={events?.totalPages}
          /> 
       </section>
     </>
--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,6 +1,9 @@
 import { IEvent } from '@/lib/database/models/event.model'
 import React from 'react'
 import Card from "./Card"
+import Pagination from './Pagination'
+ 
+
 
 type CollectionProps = {
   data: IEvent[],
@@ -28,7 +31,8 @@ const Collection = ({
         <div className='flex flex-col items-center gap-10'>
           <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10">
             {data.map((event) => {
-              const hasOrderlink = collectionType === 'Events_Organized'
+              const hasOrderlink = collectionType ===
+                'Events_Organized';
               const hidePrice = collectionType === 'My_Tickets';
   
               return (
@@ -37,8 +41,12 @@ const Collection = ({
                     hidePrice={hidePrice} />
                 </li>
               )
-           })}
-          </ul>
+            })}</ul>
+          {totalPages > 1 && (
+            <Pagination urlParamName={urlParamName} page={page}
+             totalPages={totalPages} />
+          )}
+
           {data[0].title}
         </div>
       ): (
